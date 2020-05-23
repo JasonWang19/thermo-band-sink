a1cb07bb4f11912ead2583bed3fef5dda97f7295
Jason Wang <jianelectric@gmail.com>
2020-05-22 17:45:53 -0700

    Add org, device, staff related functions

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const axios = require('axios');
 
 const app = express();
 
+// env
 const dbUrl = process.env.DB_URL;
 const dbName = process.env.DB_NAME;
 const PORT = process.env.PORT;
@@ -17,7 +18,12 @@ const appSecret = process.env.WX_SECRET;
 // collection names
 const dataHistories = 'data_histories';
 const userInfos = 'user_infos';
-const devices = 'devices'
+const devices = 'devices';
+const orgs = 'orgs';
+const staffs = 'staffs';
+
+// constants
+const DEFAULT_QUERY_INTERVAL = 2 * 3600 * 1000;
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
@@ -29,6 +35,9 @@ MongoClient.connect(dbUrl, { useUnifiedTopology: true })
         const dataHistoriesCol = db.collection(dataHistories);
         const userInfosCol = db.collection(userInfos);
         const devicesCol = db.collection(devices);
+        const orgsCol = db.collection(orgs);
+        const staffsCol = db.collection(staffs);
+
         /* 
          *  Records 
          *  保存历史数据
@@ -74,9 +83,45 @@ MongoClient.connect(dbUrl, { useUnifiedTopology: true })
                 })
                 .catch(err => {
                     console.error(err);
-                    res.statusCode(400);
+                    res.status(400);
                 })
-            // res.statusCode(500).send('internal error');
+            // res.status(500).send('internal error');
+        });
+
+        /*
+        *  Records
+        *  GET 
+        *  获取历史数据
+       */
+        app.get('/records/c/:cId', (req, res) => {
+            const cId = req.params.cId;
+            const current = new Date();
+            const startTs = req.query.from ? new Date(parseInt(req.query.from)) : new Date(current - DEFAULT_QUERY_INTERVAL);
+            const endTs = req.query.to ? new Date(parseInt(req.query.to)) : current;
+            console.log('Request records for c id: ', cId, startTs, endTs);
+
+            dataHistoriesCol.find({
+                c: cId,
+                ts: { $gte: startTs, $lt: endTs }
+            }).toArray((err, docs) => {
+                if (err) {
+                    res.status(500).end();
+                }
+                if (!docs) {
+                    res.status(400).end();
+                }
+                res.json({
+                    c: cId,
+                    i: docs[0].i,
+                    r: docs.map(doc => {
+                        return {
+                            ts: doc.ts,
+                            ti: doc.ti
+                        }
+                    })
+                });
+            });
+
         });
 
         /* 
@@ -131,10 +176,10 @@ MongoClient.connect(dbUrl, { useUnifiedTopology: true })
         });
 
         /*
-         *  Staff user info
-         *  员工用户(教职员工)信息的更新
+         *  Staff user weixin info
+         *  员工用户(教职员工)微信信息的更新
         */
-        app.post('/staff', (req, res) => {
+        app.post('/staffUser', (req, res) => {
             const data = req.body;
             console.log("request body", data);
             const openid = data.openid;
@@ -163,6 +208,144 @@ MongoClient.connect(dbUrl, { useUnifiedTopology: true })
         });
 
         /*
+        *  Staff user detail info
+        *  员工用户(教职员工)信息的更新
+        */
+        app.post('/staff', (req, res) => {
+            const data = req.body;
+            console.log("request body", data);
+            const staffId = data.staffId;
+            staffsCol.updateOne(
+                {
+                    staffId
+                },
+                {
+                    $set: {
+                        ...data
+                    }
+                },
+                {
+                    upsert: true
+                }
+            )
+                .then(result => {
+                    res.json(data);
+                })
+                .catch(err => {
+                    console.error('err', err);
+                    res.status(400).json({
+                        errmsg: 'cannot persist staff info'
+                    })
+                });
+        });
+
+        /*
+         *  Staff user detail info
+         *  GET 
+         *  获取员工用户(教职员工)信息
+        */
+        app.get('/staff/:staffId', (req, res) => {
+            const staffId = req.params.staffId;
+            console.log('Request for device information for staff id: ', staffId);
+
+            staffsCol.findOne({
+                staffId
+            },
+                (err, doc) => {
+                    if (err) {
+                        console.error('Query failed when extract staff info via staff id:', staffId, err);
+
+                        // TODO set error response
+                        res.status(500).end();
+                    }
+                    if (doc) {
+                        res.json(doc);
+                    } else {
+                        res.status(404).end();
+                    }
+
+                })
+        });
+
+        /*
+         *  Staff / user relation
+         *  GET 
+         *  获取员工用户的下属信息
+        */
+        app.get('/staff/:staffId/userlist', (req, res) => {
+            const staffId = req.params.staffId;
+            console.log('Request for user listfor staff id: ', staffId);
+
+            const promise = staffsCol.findOne({
+                staffId
+            })
+                .then(doc => {
+                    if (!doc || !doc.orgId) {
+                        return Promise.reject('empty doc');
+                    }
+
+                    return orgsCol.findOne({
+                        orgId: doc.orgId
+                    })
+
+                });
+            getStaffUserList(promise, staffId, res);
+
+        });
+
+        /*
+         *  Staff / user relation
+         *  GET 
+         *  获取员工用户的下属信息
+        */
+        app.get('/staff/:staffId/org/:orgId/userlist', (req, res) => {
+            const staffId = req.params.staffId;
+            const orgId = req.params.orgId;
+            console.log('Request for user listfor staff/org id: ', staffId, orgId);
+
+            const promise = orgsCol.findOne({
+                orgId
+            });
+
+            getStaffUserList(promise, staffId, res)
+
+
+        });
+
+        const getStaffUserList = (promise, staffId, res) => {
+            promise.then(doc => {
+                if (!doc) {
+                    return Promise.reject('empty doc');
+                }
+
+                const tmp = doc.map[staffId]['leaves'].map(leaf => console.log('leaf ', leaf));
+                const promises = Promise.all(doc.map[staffId]['leaves'].map(leaf => devicesCol.findOne({
+                    c: leaf
+                })));
+                return promises;
+                // return Promise.all(doc.map[staffId]['leaves'].map(leaf => devicesCol.findOne({
+                //     c: leaf
+                // })));
+            })
+                .then(docs => {
+                    if (!docs) {
+                        return Promise.reject('empty doc');
+                    }
+                    return res.json(docs);
+                })
+                .catch(err => {
+                    if (err === 'empty doc') {
+                        res.status(404).end();
+                    } else {
+                        console.error('Query failed when extract staff info via staff id:', staffId, err);
+
+                        // TODO set error response
+                        res.status(500).end();
+                    }
+
+                })
+        }
+        /*
          *  Device info
          *  GET
          *  获取手环相关信息
@@ -170,21 +353,22 @@ MongoClient.connect(dbUrl, { useUnifiedTopology: true })
         app.get('/device/:deviceId', (req, res) => {
             console.log('Request for device information for device id: ', req.params.deviceId);
             const deviceId = req.params.deviceId;
-            devicesCol.find({
+            devicesCol.findOne({
                 deviceId
-            })
-                .toArray((err, rels) => {
+            },
+                (err, doc) => {
                     if (err) {
-                        console.error('Query failed when extract device via device id:', deviceId);
+                        console.error('Query failed when extract device via device id:', deviceId, err);
+
                         // TODO set error response
-                    } else if (rels.length > 1) {
-                        console.error('Data setup issue, one device should have only one record, device id:', deviceId)
-                    } else if (rels.length < 1) {
-                        res.status(404);
-                        // TODO return common 404
-                    } else {
-                        res.json(rels[0]);
+                        res.status(500).end();
                     }
+                    if (doc) {
+                        res.json(doc);
+                    } else {
+                        res.status(404).end();
+                    }
+
                 })
         });
 
@@ -197,40 +381,121 @@ MongoClient.connect(dbUrl, { useUnifiedTopology: true })
             console.log('Request for update device information: ', req.body);
             const data = req.body;
             const deviceId = data.deviceId;
-            devicesCol.count({ deviceId })
-                .then(number => {
-                    if (number > 1) {
-                        res.status(400).json({
-                            errmsg: `Data setup issue, more than one recrods for deviceId: ${deviceId}`
-                        });
-                        return;
+
+            devicesCol.updateOne(
+                {
+                    deviceId
+                },
+                {
+                    $setOnInsert: {
+                        ...data
                     }
-                    devicesCol.updateOne(
-                        {
-                            deviceId
-                        },
-                        {
-                            $setOnInsert: {
-                                ...data
-                            }
-                        },
-                        {
-                            upsert: true
-                        }
-                    )
-                        .then(result => {
-                            res.json(data);
-                        })
-                        .catch(err => {
-                            console.error('err', err);
-                            res.status(400).json({
-                                errmsg: `cannot setup device info for device id: ${deviceId}`
-                            })
-                        });
+                },
+                {
+                    upsert: true
+                }
+            )
+                .then(result => {
+                    res.json(data);
                 })
+                .catch(err => {
+                    console.error('err', err);
+                    res.status(400).json({
+                        errmsg: `cannot setup device info for device id: ${deviceId}`
+                    })
+                });
 
         });
 
+        /*
+         *  Org info
+         *  POST
+         *  更新班级相关信息
+        */
+        app.post('/org', (req, res) => {
+            console.log('Request for update org information: ', req.body);
+            const data = req.body;
+            const orgId = data.ordId;
+
+            orgsCol.updateOne(
+                {
+                    orgId
+                },
+                {
+                    $setOnInsert: {
+                        ...data
+                    }
+                },
+                {
+                    upsert: true
+                }
+            )
+                .then(result => {
+                    res.json(data);
+                })
+                .catch(err => {
+                    console.error('err', err);
+                    res.status(400).json({
+                        errmsg: `cannot setup org info for org id: ${orgId}`
+                    })
+                });
+
+        });
+
+        /*
+         *  Org info by id
+         *  GET
+         *  通过Org ID获取班级相关信息
+        */
+        app.get('/org/id/:orgId', (req, res) => {
+            console.log('Request for obtain org information: ', req.params.orgId);
+
+            const orgId = req.params.orgId;
+
+            orgsCol.findOne(
+                { orgId },
+                (err, doc) => {
+                    if (err) {
+                        console.error('Query failed when extract org via org id:', orgId);
+                        res.status(500).end();
+                    }
+                    if (doc) {
+                        res.json(doc);
+                    } else {
+                        res.status(404).end();
+                    }
+
+                }
+            )
+
+        });
+        /*
+         *  Org info by name
+         *  GET
+         *  通过Org name获取班级相关信息
+        */
+        app.get('/org/name/:orgName', (req, res) => {
+            console.log('Request for obtain org information: ', req.params.orgName);
+
+            const orgName = req.params.orgName;
+
+            orgsCol.findOne(
+                { orgName },
+                (err, doc) => {
+                    if (err) {
+                        console.error('Query failed when extract org via org name:', orgName);
+                        res.status(500).end();
+                    }
+                    if (doc) {
+                        res.json(doc);
+                    } else {
+                        res.status(404).end();
+                    }
+
+                }
+            )
+
+        });
 
     }).catch(err => console.error(err));
 
