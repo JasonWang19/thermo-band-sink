839377fe0b24bb4f2cc8e0d4d73fcedece5fad99
Jason Wang <jianelectric@gmail.com>
2020-07-12 11:59:43 -0700

    Add signup and username check

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,6 +2,25 @@ const router = require('express').Router();
 const auth = require('./auth');
 const passport = require('passport');
 const { Users } = require('../models/Users');
+const { Connection: conn } = require('../utils/Connection');
+
+async function createUser(user, res) {
+    const finalUser = new Users(user);
+
+    const u = await Users.findUser(finalUser);
+    if (u) {
+        return res.status(409).json({
+            errors: {
+                username: 'already exist',
+            }
+        })
+    }
+    finalUser.setPassword(user.password);
+
+    return finalUser.save()
+        .then(() => res.json({ user: finalUser.authJson }));
+
+}
 
 //POST new user route (optional, everyone has access)
 router.post('/', auth.optional, (req, res, next) => {
@@ -23,12 +42,43 @@ router.post('/', auth.optional, (req, res, next) => {
         });
     }
 
+    createUser(user, res);
+});
+
+//POST new user route (optional, everyone has access)
+router.post('/signup', auth.optional, (req, res, next) => {
+    const { body: { user } } = req;
+
+    if (!user.username) {
+        return res.status(422).json({
+            errors: {
+                username: 'is required',
+            },
+        });
+    }
+
+    if (!user.password) {
+        return res.status(422).json({
+            errors: {
+                password: 'is required',
+            },
+        });
+    }
+
     const finalUser = new Users(user);
 
-    finalUser.setPassword(user.password);
-
-    return finalUser.save()
-        .then(() => res.json({ user: finalUser.authJson }));
+    conn.staffsCol.findOne({ staffId: user.username })
+        .then(staff => {
+            if (!staff) {
+                return res.status(400).json({
+                    errors: {
+                        msg: 'can only signup for onboarded staff',
+                        code: 10001
+                    },
+                });
+            }
+            createUser(user, res);
+        })
 });
 
 //POST login route (optional, everyone has access)
@@ -79,6 +129,18 @@ router.get('/current', auth.required, (req, res, next) => {
 
             return res.json({ user });
         });
+});
+
+//GET check username availability
+router.get('/username/:username', auth.required, (req, res, next) => {
+    const username = req.params.username;
+
+    Users.findUser({ username })
+        .then(u => {
+            return res.json({
+                exist: u ? true : false
+            })
+        })
 });
 
 //GET current route (required, only authenticated users have access)
